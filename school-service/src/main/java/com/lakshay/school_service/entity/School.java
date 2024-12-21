f4e0e20800775ec5735804807ee4542ef68ea480
Lakshay <lakshay.rv@gmail.com>
2024-12-21 12:07:56 -0500

    school entity added

---
+++ school-service/src/main/java/com/lakshay/school_service/entity/School.java
@@ -0,0 +1,22 @@
+package com.lakshay.school_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "school")
+public class School {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String schoolName;
+    private String location;
+    private String principalName;
+}
